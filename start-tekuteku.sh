02bdc6709fa858dfe63ad271216a87d9b86cfdca
Koichi Kodama <kkodama@auecc.aichi-edu.ac.jp>
2024-05-10 22:27:42 +0900

    update

--- start-tekuteku.sh
+++ start-tekuteku.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/sien/tekuteku-note/boost_1_84_0/stage/lib
+(
+cd /home/sien/tekuteku-note
 x=`/etc/apache2/ssl.key/pass_ssl.sh`
 nohup ./tekuteku_server --port 8080 --ssl /etc/apache2/ssl.key/sien.key /etc/apache2/ssl.crt/sien.aichi-edu.ac.jp.cer /etc/apache2/ssl.crt/nii-odca4g7rsa.cer $x 1>/dev/null 2>&1 &
 nohup ./tekuteku_server --port 8081 --ssl /etc/apache2/ssl.key/sien.key /etc/apache2/ssl.crt/sien.aichi-edu.ac.jp.cer /etc/apache2/ssl.crt/nii-odca4g7rsa.cer $x 1>/dev/null 2>&1 &
 nohup ./tekuteku_server --port 8082 --ssl /etc/apache2/ssl.key/sien.key /etc/apache2/ssl.crt/sien.aichi-edu.ac.jp.cer /etc/apache2/ssl.crt/nii-odca4g7rsa.cer $x 1>/dev/null 2>&1 &
 nohup ./tekuteku_server --port 8083 --ssl /etc/apache2/ssl.key/sien.key /etc/apache2/ssl.crt/sien.aichi-edu.ac.jp.cer /etc/apache2/ssl.crt/nii-odca4g7rsa.cer $x 1>/dev/null 2>&1 &
 nohup ./tekuteku_server --port 8084 --ssl /etc/apache2/ssl.key/sien.key /etc/apache2/ssl.crt/sien.aichi-edu.ac.jp.cer /etc/apache2/ssl.crt/nii-odca4g7rsa.cer $x 1>/dev/null 2>&1 &
+)
 ps -e | grep tekuteku_server
+
