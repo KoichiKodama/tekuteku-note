c6d6a4c3be9799784ac0c07b823fab3c8af58a9e
Koichi Kodama <kkodama@auecc.aichi-edu.ac.jp>
2025-06-24 13:14:11 +0900

    update

---
+++ export-git.bat
@@ -0,0 +1,3 @@
+@echo off
+if exist tekuteku-note.zip del tekuteku-note.zip
+call 7z a tekuteku-note.zip index.html tekuteku.ico てくてくノートサーバ.exe tools 1>NUL
--- export.bat
+++ export.bat
@@ -1,3 +1,13 @@
 @echo off
-if exist てくてくノート.zip del てくてくノート.zip
-call 7z a てくてくノート.zip index.html tekuteku.ico てくてくノートサーバ.exe tools 1>NUL
+mkdir export.work
+pushd export.work
+copy ..\index.html . 1>NUL
+copy ..\tekuteku.ico . 1>NUL
+copy ..\てくてくノートサーバ.exe . 1>NUL
+copy ..\config-yy.json .\config.json 1>NUL
+copy ..\yy-service.exe . 1>NUL
+copy ..\yy.ico . 1>NUL
+robocopy ..\tools .\tools /mir 1>NUL
+call 7z a -scsWIN ..\てくてくノート.zip * 1>NUL
+popd
+rmdir /s /q export.work
